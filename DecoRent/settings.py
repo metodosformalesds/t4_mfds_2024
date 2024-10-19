ffa1b2fbcb78fe9e1844c5b248aa71bca7ba5830
AlanRoblesSoto <al214723@alumnos.uacj.mx>
2024-10-19 17:28:22 -0600

    Se creo un archivo base.html y se distribuyeron los archivos css

--- DecoRent/settings.py
+++ DecoRent/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'DecoRent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Servicios', 'Template')], 
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
