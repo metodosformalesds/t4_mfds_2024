b7db35f638835c8ff1c955c5b724d8a9ce38c76a
AlanRoblesSoto <al214723@alumnos.uacj.mx>
2024-10-27 20:18:38 -0600

    Se migro la base de datos a MySQL

--- DecoRent/settings.py
+++ DecoRent/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'DecoRent.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'decorent_bd',
+        'USER': 'root',
+        'PASSWORD': 'metodosformales',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
