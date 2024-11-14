96252757a16dd56b430ae9479488af1cd7b0e528
AlanRoblesSoto <al214723@alumnos.uacj.mx>
2024-11-13 20:25:06 -0700

    Se modifico un detalle estitico de la pantalla de inicio

--- Servicios/urls.py
+++ Servicios/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('publicacion_servicio/<int:id>/', views.publicacion_servicio, name='publicacion_servicio'), 
     path('publicacion_servicio/<int:id>/delete', views.eliminar_publicacion, name='eliminar_publicacion'), 
     path('editar_servicio/<int:servicio_id>/', views.editar_servicio, name='editar_servicio'),
-<<<<<<< HEAD
     path('create_account/', views_stripe.create_account, name='create_account'),
     path('create_account_link/', views_stripe.create_account_link, name='create_account_link'),
     path('onboarding_complete/', views_stripe.stripe_onboarding_complete, name='stripe_onboarding_complete'),
@@ -16,9 +15,5 @@ urlpatterns = [
     path('payment_success/', views_stripe.payment_success, name='payment_success'),
     path('payment_cancel/', views_stripe.payment_cancel, name='payment_cancel'),
     path('stripe_dashboard/', views_stripe.stripe_dashboard_link, name='stripe_dashboard_link'),
-]
-=======
     path('servicio/<int:servicio_id>/agregar_reseña/', views.agregar_reseña, name='agregar_reseña'),
-    
 ]
->>>>>>> d116f1b
